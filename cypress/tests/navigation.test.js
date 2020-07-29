cb4538069c5afe8b2572044336a3b6cb57b684bb
j.langa <juan.langa@ynap.com>
2020-07-29 12:27:05 +0100

    Scoring Rules page 2

--- cypress/tests/navigation.test.js
+++ cypress/tests/navigation.test.js
@@ -8,9 +8,11 @@ describe('Navigation', () => {
         checkNavigationItemLabel(1, 'Inicio')
         checkNavigationItemLabel(2, 'Participantes')
         checkNavigationItemLabel(3, 'Resultados')
+        checkNavigationItemLabel(4, 'Scoring Rules')
 
         checkNavigationItemLink(2, 'participants')
         checkNavigationItemLink(3, 'results')
+        checkNavigationItemLink(4, 'scoring-rules')
         checkNavigationItemLink(1, '')
     })
 
