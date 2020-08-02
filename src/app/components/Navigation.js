430980f661477e70ee720d603c00f24b355cd8ff
j.langa <juan.langa@ynap.com>
2020-08-02 17:10:38 +0100

    sticky navigation

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { ConnectedLanguagePicker } from './LanguagePicker'
 
 const Navigation = ({ translations }) => (
-    <nav className="navbar navbar-expand-lg navbar-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-white sticky-top">
         <ConnectedLanguagePicker />
         <div className="pr-5"></div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" data-automation="mobile-nav-toggle-button">
