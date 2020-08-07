35324ee628f9e4c2f51105ea58903cc18db1c4a9
j.langa <juan.langa@ynap.com>
2020-08-07 16:09:28 +0100

    refactoring and adding marging to nav items for desktop view

--- src/app/components/PredictionsForm.js
+++ src/app/components/PredictionsForm.js
@@ -87,12 +87,11 @@ const PredictionsForm = ( {
                 
                 {userID !== "U1" ?
                     <div>
-                        
-                                <div className="form-group pt-3 px-2 col-md-6 offset-md-3">
-                                    {`${translations.predictionsForm.username}: `}
-                                    <input type="text" onChange={e => setUsernameHandler(predictionType, predictionDetails(predictionType).userID, e)} value={predictionDetails(predictionType).username} className="form-control" data-automation="username-input"/>
-                                </div>
-                        {/* </div> */}
+                        <div className="form-group pt-3 px-2 col-md-6 offset-md-3">
+                            {`${translations.predictionsForm.username}: `}
+                            <input type="text" onChange={e => setUsernameHandler(predictionType, predictionDetails(predictionType).userID, e)} value={predictionDetails(predictionType).username} className="form-control" data-automation="username-input"/>
+                        </div>
+
                         {!generatingRandomPredictions ?
                             <div className="text-center py-3">
                                 <button 
