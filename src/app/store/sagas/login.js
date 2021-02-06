b85eac84618c6077ba34e34a33acb00c2c78968c
J.Langa <Juan.Langa@ynap.com>
2021-02-06 15:25:51 +0000

    Sign In error messages

--- src/app/store/sagas/login.js
+++ src/app/store/sagas/login.js
@@ -1,9 +1,11 @@
-import { take, put } from 'redux-saga/effects'
+import { take, put, select } from 'redux-saga/effects'
 import axios from 'axios'
-import { history } from '../history'
+import * as selectors from '../selectors'
 import * as mutations from '../mutations'
+import { history } from '../history'
 import { url } from './url'
 import { getLocalStorageUser, setLocalStorageUser } from '../../../utils/common'
+import { userExists } from '../../../utils/login'
 
 export function* getSessionStatusSaga() {
     while (true) {
@@ -27,19 +29,35 @@ export function* userAuthenticationSaga(){
     while (true){
         const { username, passwordHash } = yield take(mutations.REQUEST_AUTHENTICATE_USER)
 
-        try {
-            const { data } = yield axios.post(url + '/authenticate', { username, passwordHash })
-            const { authenticated, id, idToken } = data.session
+        let allUsers = yield select(selectors.getUsers)
 
-            yield put(mutations.processAuthenticateUser(authenticated, id))
+        if ( username == '') {
+            yield put(mutations.hideEmailNotRegisteredMessage())
             yield put(mutations.hideIncorrectPasswordMessage())
-
-            setLocalStorageUser(idToken.token)
-
-            history.push('/account')
-        } catch (e) {
-            yield put(mutations.processAuthenticateUser(mutations.NOT_AUTHENTICATED))
-            yield put(mutations.showIncorrectPasswordMessage())
+            yield put(mutations.showNoEmailSignInMessage())
+        } else if (!userExists(allUsers.allIds, username)) {
+            yield put(mutations.hideNoEmailSignInMessage())
+            yield put(mutations.hideIncorrectPasswordMessage())
+            yield put(mutations.showEmailNotRegisteredMessage())
+        } else {
+            try {
+                const { data } = yield axios.post(url + '/authenticate', { username, passwordHash })
+                const { authenticated, id, idToken } = data.session
+    
+                yield put(mutations.processAuthenticateUser(authenticated, id))
+                yield put(mutations.hideNoEmailSignInMessage())
+                yield put(mutations.hideEmailNotRegisteredMessage())
+                yield put(mutations.hideIncorrectPasswordMessage())
+    
+                setLocalStorageUser(idToken.token)
+    
+                history.push('/account')
+            } catch (e) {
+                yield put(mutations.processAuthenticateUser(mutations.NOT_AUTHENTICATED))
+                yield put(mutations.hideNoEmailSignInMessage())
+                yield put(mutations.hideEmailNotRegisteredMessage())
+                yield put(mutations.showIncorrectPasswordMessage())
+            }
         }
     }
 }
