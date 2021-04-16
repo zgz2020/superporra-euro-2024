b0a8cdb35f78649927b2bd1e2556627c61987a65
J.Langa <Juan.Langa@ynap.com>
2021-04-16 12:40:47 +0100

    fix

--- src/app/store/reducers/predictions.js
+++ src/app/store/reducers/predictions.js
@@ -41,7 +41,7 @@ export const predictions = (predictions = defaultState.predictions, action) => {
                     ...predictions.byId,
                     [action.predictionID]: { 
                         ...predictions.byId[action.predictionID],
-                        privateLeague: [action.privateLeague] 
+                        privateLeague: action.privateLeague
                     }
                 }
             }
