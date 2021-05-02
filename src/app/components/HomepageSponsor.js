66616f263b2e2be1affb111ab212016d985f5dfd
J.Langa <Juan.Langa@ynap.com>
2021-05-02 20:42:27 +0100

    images update

--- src/app/components/HomepageSponsor.js
+++ src/app/components/HomepageSponsor.js
@@ -6,7 +6,7 @@ const HomepageSponsor = ({ translations }) => (
         <div className="card-body text-center">
             {translations.homepage.sponsoredBy}
         </div>
-        <img className="card-img-bottom px-1" src="./images/JLD-Banner-2.png" alt="Jelen Landon Designs"></img>
+        <img className="card-img-bottom px-1" src="src/images/JLD-Banner-2.png" alt="Jelen Landon Designs"></img>
     </div>
 )
 
