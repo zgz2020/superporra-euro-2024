17759e011db6ca392beb7b935b9e47bc42d55e71
J.Langa <Juan.Langa@ynap.com>
2021-05-02 20:53:48 +0100

    image update

--- src/app/components/HomepageSponsor.js
+++ src/app/components/HomepageSponsor.js
@@ -6,7 +6,7 @@ const HomepageSponsor = ({ translations }) => (
         <div className="card-body text-center">
             {translations.homepage.sponsoredBy}
         </div>
-        <img className="card-img-bottom px-1" src="src/images/JLD-Banner-2.png" alt="Jelen Landon Designs"></img>
+        <img className="card-img-bottom px-1" src="https://res.cloudinary.com/hjsnqmtbi/image/upload/v1619984978/JLD-Banner-2_copy_siuzdf.png" alt="Jelen Landon Designs"></img>
     </div>
 )
 
