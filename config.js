3a74002c2af40cbec7093dc5156cc466aafdf408
J.Langa <Juan.Langa@ynap.com>
2021-05-06 13:28:38 +0100

    config value update

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://devAdmin:miclave@cluster0.z4s3d.mongodb.net/dev?retryWrites=true&w=majority' 
+export const DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://testUser:testPassword@cluster0.z4s3d.mongodb.net/dev?retryWrites=true&w=majority' 
