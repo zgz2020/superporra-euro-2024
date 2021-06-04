1c65aa1f370641995a0df5e10eee68addab4eee6
J.Langa <Juan.Langa@ynap.com>
2021-06-04 14:16:47 +0100

    Comment module - improvements

--- src/app/components/CommentsBox.js
+++ src/app/components/CommentsBox.js
@@ -2,6 +2,54 @@ import React from 'react'
 import { connect } from 'react-redux'
 import * as mutations from '../store/mutations'
 
+
+const commentsForm = (submitHandler, username, translations, postCommentSuccess, postCommentError) => (
+    <form onSubmit={e => submitHandler(username, e)} className="mx-3 mb-3">
+
+        {!username ?
+            <input 
+                className="form-control col-sm-6 col-md-5 col-lg-4 col-xl-3 mb-2" 
+                name="username"
+                placeholder={translations.comments.placeholderUsername}
+                id="usernameInput"
+            />
+            : null
+        }
+        
+        <textarea 
+            className="form-control" 
+            name="comment"
+            placeholder={translations.comments.placeholderText}
+            id="commentInput" rows="3" 
+        />
+
+        {postCommentSuccess &&  
+            <p 
+                className="text-success font-italic mt-2"
+                data-automation={'comment-success'}
+            >
+                {translations.comments.success}
+            </p>
+        }
+
+        {postCommentError &&  
+            <p 
+                className="text-danger font-italic mt-2"
+                data-automation={'comment-error'}
+            >
+                {translations.comments.error}
+            </p>
+        }
+
+        <button 
+            type="submit" 
+            className="form-control mt-2 btn btn-outline-secondary col-3 col-sm-3 col-md-3 col-lg-2 col-xl-2"
+        >
+            {translations.accountPage.submit}
+        </ button>
+    </form>
+)
+
 const CommentsBox = ({ comments, submitHandler, username, postCommentSuccess, postCommentError, translations }) => (
     <div className="card mb-5">
         <div className="card-header bg-dark text-white">
@@ -9,60 +57,25 @@ const CommentsBox = ({ comments, submitHandler, username, postCommentSuccess, po
         </div>
 
         <div className="card-body">
+            {comments.length > 10 ?
+                commentsForm(submitHandler, username, translations, postCommentSuccess, postCommentError)
+                : null
+            }
+
             {comments.reverse().map((comment, index) => (
                 <div key={index} className="card-body">
-                    {comment.username} - {comment.date}
-                    <br />
+                    <div className="font-italic">
+                        {comment.username} - {comment.date}
+                    </div>
                     {comment.text}
                 </div>
             ))}
-        </div>
 
-        <form onSubmit={e => submitHandler(username, e)} className="mx-3 mb-3">
-
-            {!username ?
-                <input 
-                    className="form-control col-sm-6 col-md-4 col-lg-3 col-xl-3 mb-2" 
-                    name="username"
-                    placeholder={translations.comments.placeholderUsername}
-                    id="usernameInput"
-                />
+            {comments.length <= 10 ?
+                commentsForm(submitHandler, username, translations, postCommentSuccess, postCommentError)
                 : null
             }
-            
-            <textarea 
-                className="form-control" 
-                name="comment"
-                placeholder={translations.comments.placeholderText}
-                id="commentInput" rows="3" 
-            />
-
-            {postCommentSuccess &&  
-                <p 
-                    className="text-success font-italic mt-2"
-                    data-automation={'comment-success'}
-                >
-                    {translations.comments.success}
-                </p>
-            }
-
-            {postCommentError &&  
-                <p 
-                    className="text-danger font-italic mt-2"
-                    data-automation={'comment-error'}
-                >
-                    {translations.comments.error}
-                </p>
-            }
-
-            <button 
-                type="submit" 
-                className="form-control mt-2 btn btn-outline-secondary col-3 col-sm-3 col-md-3 col-lg-2 col-xl-2"
-            >
-                {translations.accountPage.submit}
-            </ button>
-        </form>
-
+        </div>
     </div>
 )
 
