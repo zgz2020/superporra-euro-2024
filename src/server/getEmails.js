b7ee2411cbac62e778b89017469490acd53be13f
J.Langa <Juan.Langa@ynap.com>
2021-06-09 15:16:12 +0100

    get emails to send updates

---
+++ src/server/getEmails.js
@@ -0,0 +1,21 @@
+import { MongoClient } from 'mongodb'
+
+const url = "mongodb+srv://juanjoAdmin:Lidon2020@cluster0.tkptx.mongodb.net/production?retryWrites=true&w=majority"
+let db = null
+
+async function connectDB(){
+    if (db) return db
+    let client = await MongoClient.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true })
+    db = client.db()
+    return db
+}
+
+
+(async function getEmails(){
+    let db = await connectDB();
+    let users = await db.collection('users').find().toArray()
+
+    let emails = users.map(user => user.id)
+
+    console.log('++++ EMAILS: ' + emails)
+})();
